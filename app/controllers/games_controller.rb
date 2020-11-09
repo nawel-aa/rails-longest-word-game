849318b107444c14272fa7e3182eef10da168c4c
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-09 14:45:33 -0500

    score page displays result correctly

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     alphabet = ("A".."Z").to_a
@@ -6,5 +9,28 @@ class GamesController < ApplicationController
   end
 
   def score
+    return @result = :wrong_letters unless word_uses_letters?
+
+    @result = word_exists? ? :won : :wrong_word
+  end
+
+  private
+
+  def word_uses_letters?
+    word = params[:word].split('')
+    letters = params[:letters].split(' ')
+    word.each do |letter|
+      letter_index = letters.index(letter.upcase)
+      return false if letter_index.nil?
+
+      letters.delete_at(letter_index)
+    end
+    true
+  end
+
+  def word_exists?
+    serialized_answer = open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read
+    answer = JSON.parse(serialized_answer)
+    answer['found']
   end
 end
